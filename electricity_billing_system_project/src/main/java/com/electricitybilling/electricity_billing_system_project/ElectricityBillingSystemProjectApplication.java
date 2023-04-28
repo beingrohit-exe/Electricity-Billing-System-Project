752e9ac3c9b4795dbf5d795769d4c8e59b7c5be3
Rohit Parihar <rohitparih@gmail.com>
2023-04-29 01:21:22 +0530

    ER Diagram

---
+++ electricity_billing_system_project/src/main/java/com/electricitybilling/electricity_billing_system_project/ElectricityBillingSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.electricitybilling.electricity_billing_system_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBillingSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBillingSystemProjectApplication.class, args);
+	}
+
+}
---
+++ electricity_billing_system_project/src/test/java/com/electricitybilling/electricity_billing_system_project/ElectricityBillingSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electricitybilling.electricity_billing_system_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityBillingSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
