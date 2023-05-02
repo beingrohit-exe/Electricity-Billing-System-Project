b5a69fb83486e5ce1688c2573ff1c1de2102012f
beingrohit25 <rohitparih@gmail.com>
2023-05-03 00:12:24 +0530

    Customer-Entity-to-Dto-Conversion-with-ModelMapper

--- electricity_billing_system_project/src/main/java/com/electricitybilling/electricity_billing_system_project/Implementation/CustomerImplementation.java
+++ electricity_billing_system_project/src/main/java/com/electricitybilling/electricity_billing_system_project/Implementation/CustomerImplementation.java
@@ -1,9 +1,11 @@
 package com.electricitybilling.electricity_billing_system_project.Implementation;
 
+import com.electricitybilling.electricity_billing_system_project.Entity.Customer;
 import com.electricitybilling.electricity_billing_system_project.Payload.ApiResponse;
 import com.electricitybilling.electricity_billing_system_project.Payload.CustomerDto;
 import com.electricitybilling.electricity_billing_system_project.Repository.CustomerRepository;
 import com.electricitybilling.electricity_billing_system_project.Service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -23,9 +25,71 @@ public class CustomerImplementation implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+//    @Override
+//    public CustomerDto registerCustomer(CustomerDto customerDto) {
+//
+//        //Creating customer object so that we can set all the properties
+//        Customer customer = new Customer();
+//
+//        //Converting CustomerDto to Customer Entity
+//        customer.setFirstName(customerDto.getFirstName());
+//        customer.setMiddleName(customerDto.getMiddleName());
+//        customer.setLastName(customerDto.getLastName());
+//        customer.setEmail(customerDto.getEmail());
+//        customer.setMobileNumber(customerDto.getMobileNumber());
+//        customer.setGender(customerDto.getGender());
+//
+//        //Entry to database for Customer
+//        Customer savedCustomer = customerRepository.save(customer);
+//
+//        //Creating new CustomerDto Object
+//        CustomerDto savedDto = new CustomerDto();
+//
+//        //Converting Customer Entity to CustomerDto because our return type CustomerDto
+//        savedDto.setFirstName(savedCustomer.getFirstName());
+//        savedDto.setMiddleName(savedCustomer.getMiddleName());
+//        savedDto.setLastName(savedCustomer.getLastName());
+//        savedDto.setMobileNumber(savedCustomer.getMobileNumber());
+//        savedDto.setGender(savedCustomer.getGender());
+//        savedDto.setEmail(savedCustomer.getEmail());
+//
+//        //Returning CustomerDto
+//        return savedDto;
+//    }
+
+//    @Override
+//    public CustomerDto registerCustomer(CustomerDto customerDto) {
+//
+//        //Converting CustomerDto to Entity
+//        Customer customer = dtoToEntity(customerDto);
+//
+//        //Entry to database for Customer
+//        Customer savedCustomer = customerRepository.save(customer);
+//
+//        //Converting Entity to CustomerDto
+//        CustomerDto customer1 = customerToDto(savedCustomer);
+//
+//        //Returning CustomerDto
+//        return customer1;
+//    }
+
+    //Curl -> curl -X POST http://localhost:8000/customer -H "Content-Type:application/json" -d "{\"firstName\":\"Rohit\",\"lastName\":\"Parihar\",\"email\":\"rohit@gmail.com\",\"mobileNumber\":\"78755455\",\"gender\":\"MALE\"}"
+    //Curl Powershell ->  curl -X POST 'http://localhost:8000/customer' -H 'Content-Type:application/json' -d '{\"firstName\":\"Rohit\",\"lastName\":\"Parihar\",\"email\":\"rohit@gmail.com\",\"mobileNumber\":\"78755455\",\"gender\":\"MALE\"}'
     @Override
     public CustomerDto registerCustomer(CustomerDto customerDto) {
-        return null;
+
+        Customer customer = modelMapper.map(customerDto, Customer.class);
+
+        //Entry to database for Customer
+        Customer savedCustomer = customerRepository.save(customer);
+
+        CustomerDto customer2 = modelMapper.map(savedCustomer, CustomerDto.class);
+
+        //Returning CustomerDto
+        return customer2;
     }
 
     @Override
@@ -46,5 +110,30 @@ public class CustomerImplementation implements CustomerService {
     @Override
     public ApiResponse deleteCustomer(Long customerId) {
         return null;
+    }
+
+    private Customer dtoToEntity(CustomerDto customerDto){
+        Customer customer = new Customer();
+        customer.setFirstName(customerDto.getFirstName());
+        customer.setMiddleName(customerDto.getMiddleName());
+        customer.setLastName(customerDto.getLastName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        customer.setGender(customerDto.getGender());
+        return customer;
+    }
+
+    private static CustomerDto customerToDto(Customer savedCustomer) {
+        //Creating new CustomerDto Object
+        CustomerDto savedDto = new CustomerDto();
+
+        //Converting Customer Entity to CustomerDto because our return type CustomerDto
+        savedDto.setFirstName(savedCustomer.getFirstName());
+        savedDto.setMiddleName(savedCustomer.getMiddleName());
+        savedDto.setLastName(savedCustomer.getLastName());
+        savedDto.setMobileNumber(savedCustomer.getMobileNumber());
+        savedDto.setGender(savedCustomer.getGender());
+        savedDto.setEmail(savedCustomer.getEmail());
+        return savedDto;
     }
 }
